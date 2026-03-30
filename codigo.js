b5b0a2716f5542236017bd03ea2248d6f08061c5
lucaspera09 <lucaspera.2001@gmail.com>
2026-03-30 09:12:10 -0300

    ultimo commit

---
+++ codigo.js
@@ -0,0 +1,32 @@
+// ANIMACIONES
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
+
+
+// WHATSAPP FORM
+function enviarWhatsApp(e){
+  e.preventDefault();
+
+  let nombre = document.getElementById("nombre").value;
+  let telefono = document.getElementById("telefono").value;
+  let mensaje = document.getElementById("mensaje").value;
+
+  let texto = `Hola, quiero hacer una consulta:
+
+Nombre: ${nombre}
+Teléfono: ${telefono}
+Consulta: ${mensaje}`;
+
+  let url = "https://wa.me/59894304856?text=" + encodeURIComponent(texto);
+
+  window.open(url, "_blank");
+}
